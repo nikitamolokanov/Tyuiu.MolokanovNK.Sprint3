3c73cfb3d834c64ab92df602b4f9b8ab4c817306
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-23 20:26:39 +0500

    Add project files.

---
+++ Tyuiu.MolokanovNK.Sprint3.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task0.V6.Lib
+{
+    public class DataService : ISprint3Task0V6
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double MultiplySeries = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                MultiplySeries = MultiplySeries + (((1 / Math.Cos(5) + 1)));
+            }
+            return Math.Round(MultiplySeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint3.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.MolokanovNK.Sprint3.Task0.V6.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 15;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = 67.88;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
