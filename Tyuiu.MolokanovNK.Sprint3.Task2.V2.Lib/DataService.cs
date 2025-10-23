a00724eceaf98c4a7676f2d6869a011d9f15bd15
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-23 22:30:44 +0500

    Task2

---
+++ Tyuiu.MolokanovNK.Sprint3.Task2.V2.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task2.V2.Lib
+{
+    public class DataService : ISprint3Task2V2
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double MultiplySeries = 1;
+            do
+            {
+                MultiplySeries = MultiplySeries * (Math.Sin(0.1) + startValue);
+                startValue++;
+            } while ( startValue > stopValue );
+            return Math.Round(MultiplySeries, 3);
+        }   
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint3.Task2.V2.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.MolokanovNK.Sprint3.Task2.V2.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task2.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 7;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = 1.1;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
