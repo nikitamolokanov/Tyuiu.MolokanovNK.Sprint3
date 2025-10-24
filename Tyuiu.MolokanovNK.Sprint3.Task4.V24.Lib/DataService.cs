2de3334adbb50e594511468600694e39a252f55e
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-24 20:34:20 +0500

    Task4

---
+++ Tyuiu.MolokanovNK.Sprint3.Task4.V24.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task4.V24.Lib
+{
+    public class DataService : ISprint3Task4V24
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 0; 
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    res = res * (Math.Sin(x) / x) + 2;
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint3.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.MolokanovNK.Sprint3.Task4.V24.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task4.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+         
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 4.477;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
