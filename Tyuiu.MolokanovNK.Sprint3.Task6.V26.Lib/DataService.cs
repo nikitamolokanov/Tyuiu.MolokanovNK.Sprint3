b4c19b664e8f44aa36b4da1963c2430ed2a66403
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-28 00:57:42 +0500

    Task6

---
+++ Tyuiu.MolokanovNK.Sprint3.Task6.V26.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task6.V26.Lib
+{
+    public class DataService : ISprint3Task6V26
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x;
+            int sum = 0;
+            for (x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 1; d <= x; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        sum += d;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint3.Task6.V26.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.MolokanovNK.Sprint3.Task6.V26.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task6.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 15;
+            int stopValue = 22;
+
+            double res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            double wait = 242;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
