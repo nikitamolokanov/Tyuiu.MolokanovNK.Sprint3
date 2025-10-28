78f187ab7e29a96151e8886212c5ac9c1a9dc586
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-28 19:05:44 +0500

    Task7

---
+++ Tyuiu.MolokanovNK.Sprint3.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolokanovNK.Sprint3.Task7.V25.Lib
+{
+    public class DataService : ISprint3Task7V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+
+            double[] valueArray = new double[len];
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double y;
+
+                
+                if (CheckDivisionByZero(x))
+                {
+                    y = 0;
+                }
+                else
+                {
+                    
+                    y = Math.Cos(x) + (4 * x) / 2 - Math.Sin(x) * 3 * x;
+                }
+
+                valueArray[count] = Math.Round(y, 2);
+                count++;
+            }
+
+            
+            return valueArray;
+        }
+
+        private bool CheckDivisionByZero(int x)
+        {
+            
+            return false;
+        }
+    }
+    
+}
---
+++ Tyuiu.MolokanovNK.Sprint3.Task7.V25/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.MolokanovNK.Sprint3.Task7.V25.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил : Молоканов Н. К. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* Спринт #3                                                                                   *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                                            *");
+Console.WriteLine("* Задание #7                                                                                  *");
+Console.WriteLine("* Вариант #25                                                                                 *");
+Console.WriteLine("* Выполнил: Молоканов Никита Константинович | ИБКСб-25-1                                      *");
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                            *");
+Console.WriteLine("***********************************************************************************************");
+
+
+Console.WriteLine("Таблица значений функции F(x) = cos(x) + (4x)/2 - sin(x) * 3x");
+Console.WriteLine("Диапазон: [-5; 5] с шагом 1");
+Console.WriteLine("=============================================");
+Console.WriteLine("   x    |    F(x)    ");
+Console.WriteLine("--------|-------------");
+
+double[] results = ds.GetMassFunction(-5, 5);
+
+for (int i = 0; i < results.Length; i++)
+{
+    int x = -5 + i;
+    Console.WriteLine($"{x,7:F1} | {results[i],11:F6}");
+}
